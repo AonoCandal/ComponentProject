2c480de29b9d3a5f29cbc7091672daa9ffd07899
Aono <lijg@leleyuntech.com>
2017-11-23 18:07:40 +0800

    组件框架搭建 组件抽分完成 组件url待配置 Arouter添加

---
+++ app/src/main/java/com/leley/westpediatric/WPApplication.java
@@ -0,0 +1,17 @@
+package com.leley.westpediatric;
+
+import com.leley.basecommon.base.BaseApplication;
+
+/**
+ * Created by Aono on 2017/11/23.
+ * 最终的Application
+ *
+ * app壳工程是从名称来解释就是一个空壳工程，没有任何的业务代码，也不能有Activity，
+ * 必须被单独划分成一个组件，而不能融合到其他组件中
+ */
+
+public class WPApplication extends BaseApplication {
+
+
+
+}
--- module_main/src/main/java/com/leley/module_main/HomeActivity.java
+++ module_main/src/main/java/com/leley/module_main/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.leley.module_main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
